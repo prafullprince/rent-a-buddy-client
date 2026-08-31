aa5a37abfc81ad1fd3b5990ed38b2687a1a0d3e2
Prafull Prince <prafull.prince.dev@gmail.com>
2026-08-31 23:19:31 +0530

    ws_url

--- src/utills/socket.ts
+++ src/utills/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:8080", {
+const socket = io("wss://rentserver.visualcode.site", {
     autoConnect: false,
     reconnection: true,
     reconnectionDelay: 1000,
